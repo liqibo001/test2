4c733093b30689c74bb783e87ffe21f8e6c530c5
liqibo <1289758842@qq.com>
2020-12-14 20:03:03 +0800

    init

---
+++ day01/src/main/java/com/atguigu/Demo/pull_test.java
@@ -0,0 +1,8 @@
+package com.atguigu.Demo;
+
+public class pull_test {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+        System.out.println("22222222222");
+    }
+}
