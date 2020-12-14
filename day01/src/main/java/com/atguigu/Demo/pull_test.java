f4b8a3da292735c6d39757b7c26bc0e61f6bc086
liqibo <1289758842@qq.com>
2020-12-14 20:21:45 +0800

    V4

--- day01/src/main/java/com/atguigu/Demo/pull_test.java
+++ day01/src/main/java/com/atguigu/Demo/pull_test.java
@@ -5,5 +5,6 @@ public class pull_test {
         System.out.println("11111111111");
         System.out.println("22222222222");
         System.out.println("33333");
+        System.out.println("444");
     }
 }
